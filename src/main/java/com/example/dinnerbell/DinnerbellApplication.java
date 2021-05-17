ea58cdfaadc8f7ca3dff152b4c07db6fe9ec2294
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-17 17:08:03 -0500

    adding model repo and controller packages

---
+++ src/main/java/com/example/dinnerbell/DinnerbellApplication.java
@@ -0,0 +1,13 @@
+package com.example.dinnerbell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinnerbellApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DinnerbellApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/dinnerbell/DinnerbellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dinnerbell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinnerbellApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
