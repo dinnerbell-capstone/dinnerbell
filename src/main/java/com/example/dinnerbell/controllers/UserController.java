7725e6889fc3d8e9ae908915d81690ee99b98d3f
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-05-18 14:49:38 -0500

    feat: usercontroller

--- src/main/java/com/example/dinnerbell/controllers/UserController.java
+++ src/main/java/com/example/dinnerbell/controllers/UserController.java
@@ -1,4 +1,35 @@
 package com.example.dinnerbell.controllers;
 
+import com.example.dinnerbell.models.User;
+import com.example.dinnerbell.repositories.UserRepo;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class UserController {
+    private final UserRepo users;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepo users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+        return "redirect:/login";
+    }
 }
