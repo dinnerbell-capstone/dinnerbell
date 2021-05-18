4432765831d38bc812d1e6a34882745464698ea7
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-18 15:13:08 -0500

    constructors getters and setters

--- src/main/java/com/example/dinnerbell/models/RestaurantCategory.java
+++ src/main/java/com/example/dinnerbell/models/RestaurantCategory.java
@@ -21,4 +21,46 @@ public class RestaurantCategory {
     inverseJoinColumns = {@JoinColumn(name = "restaurant_id")}
   )
   private List<FavoriteRestaurant> restaurants;
+
+  public RestaurantCategory() {
+  }
+
+  public RestaurantCategory(long id, String name, List<FavoriteRestaurant> restaurants) {
+    this.id = id;
+    this.name = name;
+    this.restaurants = restaurants;
+  }
+
+  public RestaurantCategory(String name) {
+    this.name = name;
+  }
+
+  public RestaurantCategory(long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public List<FavoriteRestaurant> getRestaurants() {
+    return restaurants;
+  }
+
+  public void setRestaurants(List<FavoriteRestaurant> restaurants) {
+    this.restaurants = restaurants;
+  }
 }
