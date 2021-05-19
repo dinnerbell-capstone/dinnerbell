01190546cacd3d4d249bc41a1dfe665fc6e8685f
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-19 16:02:18 -0500

    mapping to profile

--- src/main/java/com/example/dinnerbell/controllers/UserController.java
+++ src/main/java/com/example/dinnerbell/controllers/UserController.java
@@ -1,21 +1,25 @@
 package com.example.dinnerbell.controllers;
 
 import com.example.dinnerbell.models.User;
+import com.example.dinnerbell.repositories.RestaurantRepo;
 import com.example.dinnerbell.repositories.UserRepo;
+import org.dom4j.rule.Mode;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class UserController {
     private final UserRepo userDao;
-//    private final PasswordEncoder passwordEncoder;
+    private final RestaurantRepo restaurantDao;
+//    private final PasswordEncoder passwordEncoder; then add parameter to constructor
 
-    public UserController(UserRepo userDao) {
-        this.userDao = userDao;
-    }
+  public UserController(UserRepo userDao, RestaurantRepo restaurantDao) {
+    this.userDao = userDao;
+    this.restaurantDao = restaurantDao;
+  }
+
+
 
     @GetMapping("/sign-up")
     public String showSignupForm(Model model){
@@ -30,4 +34,12 @@ public class UserController {
         userDao.save(user);
         return "redirect:/login";
     }
+
+    @GetMapping("/profile")
+    public String userProfile(Model model) {
+    //  line below is for spring security
+//      User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      return "users/profile";
+    }
+
 }
