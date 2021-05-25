75c1dd8c1396a53a0843e6295be5e851d4a5c3ae
emihhorn <emilyhopehorn@gmail.com>
2021-05-25 09:41:35 -0500

    controllers

--- src/main/java/com/example/dinnerbell/controllers/SpinnerController.java
+++ src/main/java/com/example/dinnerbell/controllers/SpinnerController.java
@@ -1,10 +1,7 @@
 package com.example.dinnerbell.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SpinnerController {
