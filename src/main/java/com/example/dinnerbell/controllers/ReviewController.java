cd38bad17147aaf884f3c8665807540dc5df557e
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-26 10:23:31 -0500

    saving to pull not complete

--- src/main/java/com/example/dinnerbell/controllers/ReviewController.java
+++ src/main/java/com/example/dinnerbell/controllers/ReviewController.java
@@ -1,10 +1,7 @@
 package com.example.dinnerbell.controllers;
 
 import com.example.dinnerbell.models.Restaurant;
-import com.example.dinnerbell.repositories.CategoryRepo;
-import com.example.dinnerbell.repositories.RestaurantRepo;
-import com.example.dinnerbell.repositories.ReviewRepo;
-import com.example.dinnerbell.repositories.UserRepo;
+import com.example.dinnerbell.repositories.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +13,18 @@ public class ReviewController {
   private final CategoryRepo categoriesdao;
   private final UserRepo usersdao;
   private final ReviewRepo reviewDao;
-  private final
+  private final ImageRepo imageDao;
+
+  public ReviewController(RestaurantRepo restaurantsdao, CategoryRepo categoriesdao, UserRepo usersdao, ReviewRepo reviewDao, ImageRepo imageDao) {
+    this.restaurantsdao = restaurantsdao;
+    this.categoriesdao = categoriesdao;
+    this.usersdao = usersdao;
+    this.reviewDao = reviewDao;
+    this.imageDao = imageDao;
+  }
 
     @GetMapping("/review")
     public String reviewPage(Model model) {
-      Restaurant restaurant
-      model.addAttribute("restaurant",)
         return "post/review";
     }
 
@@ -30,5 +33,7 @@ public class ReviewController {
 
       return "business/details";
     }
+
+    private void
 
 }
