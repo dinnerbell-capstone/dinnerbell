135ceb769c98e10640eb704fde92557ef821faa7
Ann Michelle Gonzales <ann.michelle.gonzales112@gmail.com>
2021-05-27 10:18:31 -0500

    user edit functionality

--- src/main/java/com/example/dinnerbell/controllers/UserController.java
+++ src/main/java/com/example/dinnerbell/controllers/UserController.java
@@ -3,9 +3,11 @@ package com.example.dinnerbell.controllers;
 import com.example.dinnerbell.models.User;
 import com.example.dinnerbell.repositories.RestaurantRepo;
 import com.example.dinnerbell.repositories.UserRepo;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -14,20 +16,20 @@ public class UserController {
     private final RestaurantRepo restaurantDao;
     private final PasswordEncoder passwordEncoder;
 
-  public UserController(UserRepo userDao, RestaurantRepo restaurantDao, PasswordEncoder passwordEncoder) {
-    this.userDao = userDao;
-    this.restaurantDao = restaurantDao;
-    this.passwordEncoder = passwordEncoder;
-  }
+    public UserController(UserRepo userDao, RestaurantRepo restaurantDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.restaurantDao = restaurantDao;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("/sign-up")
-    public String showSignupForm(Model model){
+    public String showSignupForm(Model model) {
         model.addAttribute("user", new User());
         return "users/sign-up";
     }
 
     @PostMapping("/sign-up")
-    public String saveUser(@ModelAttribute User user){
+    public String saveUser(@ModelAttribute User user) {
         String hash = passwordEncoder.encode(user.getPassword());
         user.setPassword(hash);
         userDao.save(user);
@@ -36,9 +38,24 @@ public class UserController {
 
     @GetMapping("/profile")
     public String userProfile(Model model) {
-//      User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
-      return "users/profile";
+        return "users/profile";
+    }
+
+
+    @GetMapping("/users/{id}/edit")
+    public String edit(@PathVariable long id, Model vModel) {
+//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User userToEdit = userDao.getOne(id);
+        vModel.addAttribute("user", userToEdit);
+        return "users/edit-user-profile";
+    }
+
+    @PostMapping("/users/{id}/edit")
+    public String update(@ModelAttribute User user) {
+        userDao.save(user);
+        return "redirect:users/profile";
     }
 
 }
