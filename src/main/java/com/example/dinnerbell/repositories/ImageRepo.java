e66274e11e3d007c5f655fb62666df17a471ce2d
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-06-01 08:24:01 -0500

    custom query for now

--- src/main/java/com/example/dinnerbell/repositories/ImageRepo.java
+++ src/main/java/com/example/dinnerbell/repositories/ImageRepo.java
@@ -1,7 +1,12 @@
 package com.example.dinnerbell.repositories;
 
 import com.example.dinnerbell.models.Image;
+import com.example.dinnerbell.models.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ImageRepo extends JpaRepository<Image,Long> {
+
+  List<Image> findImageByRestaurant(Restaurant restaurant);
 }
