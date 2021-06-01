5cac4bf19140efc51d85a554fa0364c3fcd35b99
Jeanette Perez <jeanette.marie.perez@gmail.com>
2021-05-31 21:26:52 -0500

    adding custom query to find review by restaurants

--- src/main/java/com/example/dinnerbell/repositories/ReviewRepo.java
+++ src/main/java/com/example/dinnerbell/repositories/ReviewRepo.java
@@ -1,8 +1,12 @@
 package com.example.dinnerbell.repositories;
 
+import com.example.dinnerbell.models.Restaurant;
 import com.example.dinnerbell.models.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepo extends JpaRepository<Review,Long> {
 
+  List<Review> findAllByRestaurant(Restaurant restaurant);
 }
